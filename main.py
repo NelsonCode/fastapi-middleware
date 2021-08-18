7bd167719cceaa1f7897adaa8a72ed644b7fc836
Nelson-developer <nelsonher019@gmail.com>
2021-08-18 15:05:34 -0600

    Add options in CORS middleware

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-app.add_middleware(CORSMiddleware(allow_origins=["*"]))
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.middleware("http")
 async def verify_user_agent(request: Request, call_next):
